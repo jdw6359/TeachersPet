8d3de35b4cfbe29556d28545b2560c883ecaefdf
Josh Woodward <josh_woodward@medisked.com>
2015-01-05 20:22:11 -0500

    New Evaluation Interface Created

--- app/src/main/java/woodward/joshua/teacherspet/application/AddEvaluationActivity.java
+++ app/src/main/java/woodward/joshua/teacherspet/application/AddEvaluationActivity.java
@@ -1,36 +1,17 @@
 package woodward.joshua.teacherspet.application;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import woodward.joshua.teacherspet.R;
 
-public class AddEvaluationActivity extends Activity {
+public class AddEvaluationActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_evaluation);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.add_evaluation, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
